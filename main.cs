df08b7c064207057caf78172fd35f15a7645708e
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2022-10-05 14:02:34 -0400

    project 1
    yes boi

---
+++ main.cs
@@ -0,0 +1,41 @@
+using System;
+
+class Program {
+
+    public static void Main (string[] args) {
+   int num1; 
+   int num2; //Declaration
+   int num3;
+   int num4;
+   int num5;
+   int num6;
+   int num7;
+   int num8;
+  
+      int answer1;
+      int answer2;
+      int answer3;
+      int answer4;
+      int answer5;
+    num1= 10;
+    num2= 11;//Initialzation
+    num3= 12;
+    num4= 13;
+    num5= 14;
+    num6= 15;
+    num7= 16;
+    num8= 17;
+  
+    answer1= num1*num2;
+    answer2= num3-num4;
+    answer3= num5*num6;
+    answer4= num7/num8;
+    answer5= num7%num8;
+      
+  Console.WriteLine ("answer="+answer1);
+  Console.WriteLine ("answer="+answer2);
+  Console.WriteLine ("answer="+answer3);
+  Console.WriteLine ("answer="+answer4);
+  Console.WriteLine ("answer="+answer5);
+  }
+}
