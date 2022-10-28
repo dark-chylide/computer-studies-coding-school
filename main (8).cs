92af110c936894d618df33dee7da87011557a52e
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2022-10-28 14:04:29 -0400

    Add files via upload

---
+++ main (8).cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+  int i=0;
+    while(i<110)
+   {
+    Console.WriteLine ("Hello World");
+   
+  Console.WriteLine(i);
+    i++;
+    Console.WriteLine(i);
+    }  
+  }
+}
